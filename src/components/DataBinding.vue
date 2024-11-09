eb1f3cf48a7445e16947b3b852a3f4c78917c5cb
Caio <caio.batista@fattureweb.com.br>
2024-11-09 15:17:33 -0300

    formas diferentes para fazer v-on e v-bind

--- src/components/DataBinding.vue
+++ src/components/DataBinding.vue
@@ -2,7 +2,19 @@
     <div class="componente">
         <h1>Componente com Data Binding</h1>
         <p>{{ valor }}</p>
+
+        <!-- <input 
+            v-bind:value="valor"
+            v-on:input="(e)=> valor = e.target.value"
+            type="=text"
+        > -->
+        <!-- <input 
+            :value="valor" 
+            @:input="(e)=> valor = e.target.value" 
+            type="text"
+        > -->
         <input type="text" v-model="valor">
+
     </div>
 </template>
 
