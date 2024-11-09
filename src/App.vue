94ee3f2fced8875920627d00c3c729af385aee87
Caio <caio.batista@fattureweb.com.br>
2024-11-09 17:05:09 -0300

    slots

--- src/App.vue
+++ src/App.vue
@@ -11,6 +11,7 @@ import PropriedadesAssistidas from './components/PropriedadesAssistidas.vue';
 import RenderizacaoCondicional from './components/RenderizacaoCondicional.vue';
 import RenderizacaoCondicional2 from './components/RenderizacaoCondicional2.vue';
 import RenderizacaoListas from './components/RenderizacaoListas.vue';
+import Slots from './components/Slots.vue';
 
 const produto = "Camiseta";
 const preco = 49.99;
@@ -22,6 +23,18 @@ const arrayCachorros = [
 
 <template>
   <header>Vue</header>
+  <Slots>
+    <template v-slot:icone>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" width="24" height="24" stroke-width="2"> 
+        <path d="M5 12l-2 0l9 -9l9 9l-2 0"></path>
+        <path d="M5 12v7a2 2 0 0 0 2 2h10a2 2 0 0 0 2 -2v-7"></path>
+        <path d="M10 12h4v4h-4z"></path> 
+      </svg>
+    </template>
+    <template v-slot:texto>
+      Home
+    </template>
+  </Slots>
   <RenderizacaoListas nome="Francisca" :arrayCachorros="arrayCachorros"/>
   <RenderizacaoCondicional2 nome="Francisca" :qtdCachorro="10"/>
   <RenderizacaoCondicional/>
---
+++ src/components/Slots.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="componente">
+        <h1>Slots</h1>
+        <button> 
+            <slot name="icone"></slot>
+            <slot name="texto">
+                Default
+            </slot>
+        </button>
+    </div>
+</template>
