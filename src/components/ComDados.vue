a12fca531833afbfd3089393fc5b0c10d42a9bdf
Caio <caio.batista@fattureweb.com.br>
2024-11-08 21:23:40 -0300

    primeiro componente com dados

---
+++ src/components/ComDados.vue
@@ -0,0 +1,48 @@
+<template>
+    <div>
+        <h1>Componente com dados</h1>
+        <p>Nome: {{nome}} - {{ codigo }}</p>
+    </div>
+</template>
+
+<!-- <script>
+// options API
+export default {
+    data: ()=>{
+        return {
+            nome: 'Caneta1',
+            codigo: '#12ewew3'
+        }
+    }
+}
+
+// Composition API
+export default {
+    setup() {
+        const nome = "Lapis";
+        const codigo = "#123456";
+        return { nome, codigo }
+    }
+}
+</script> -->
+
+<script setup>
+
+const nome = "Apontador";
+const codigo = "#123456";
+
+</script>
+
+<style scoped>
+    div{
+        border: 2px solid rgb(10, 69, 4);
+        background-color: #050d04;
+        padding: 10px;
+        margin: 30px;
+        border-radius: 15px;
+        text-align: center;
+    }
+    p {
+        font-size: 1.5rem;
+    }
+</style>
