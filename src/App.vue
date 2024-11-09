f9057b18ebc28619d28130c31506b755b8babb6f
Caio <caio.batista@fattureweb.com.br>
2024-11-09 09:02:49 -0300

    componente com dados reativos nao primitivos

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,12 @@
 import PrimeiroComponente from './components/PrimeiroComponente.vue';
 import ComDados from './components/ComDados.vue';
 import ComDadosReativosPrimitivos from './components/DadosReativosPrimitivos.vue';
+import ComDadosReativosNaoPrimitivos from './components/DadosReativosNaoPrimitivos.vue';
 </script>
 
 <template>
   <header>Vue</header>
+  <ComDadosReativosNaoPrimitivos/>
   <ComDadosReativosPrimitivos/>
   <ComDados/>
   <PrimeiroComponente/>
---
+++ src/components/DadosReativosNaoPrimitivos.vue
@@ -0,0 +1,25 @@
+<template lang="">
+    <div class='componente'>
+        <h1>Componente Com Dados Reativos Não Primitivos</h1>
+        <p>Nome: {{pessoa.nome}} | Idade: {{pessoa.idade}}</p>
+        <button @click="fazerAniversario">Fazer Aniversário</button>
+        <button @click="mudarNome">Mudar Nome</button>
+    </div>
+</template>
+<script setup>
+import { reactive } from 'vue';
+const pessoa = reactive({
+    nome: 'Fulano',
+    idade: 30
+});
+const fazerAniversario = () => {
+    pessoa.idade++;
+}
+const mudarNome = () => {
+    pessoa.nome = 'João';
+}
+</script>
+
+<style>
+
+</style>
